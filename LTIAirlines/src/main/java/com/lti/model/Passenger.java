6ccd1f137e697877daf90b08e0a6bdeb256024a1
Deepak <Deepak@DEEPAK>
2021-09-15 13:21:25 +0530

    Pushing to branch 2

--- LTIAirlines/src/main/java/com/lti/model/Passenger.java
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -2,9 +2,27 @@ package com.lti.model;
 
 public class Passenger {
 	String passengerName;
+	int passengerId;
+	int age;
+	public int getPassengerId() {
+		return passengerId;
+	}
+
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	public String getPassengerName() {
 		return passengerName;
+		
 	}
 
 	public void setPassengerName(String passengerName) {

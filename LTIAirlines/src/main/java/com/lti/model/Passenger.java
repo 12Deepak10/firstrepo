b6242994d4a5be58cf687fe85ed9562042b0d215
Deepak <Deepak@DEEPAK>
2021-09-15 13:17:08 +0530

    Started with passenger class in branch

---
+++ LTIAirlines/src/main/java/com/lti/model/Passenger.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Passenger {
+	String passengerName;
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+}
